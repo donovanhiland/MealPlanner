8d8d67ca404bb802c9f1be12335fe54ad6fc2f88
Donovan Hiland <donovan.hiland@gmail.com>
2017-02-08 12:24:20 -0700

    cleaned some things up

--- src/server/config/environment/development.js
+++ src/server/config/environment/development.js
@@ -1,4 +1,3 @@
 export default {
   // Put your development configuration here
-  SESSION_SECRET: 'aig8las820lsdymnz0',
 };
--- src/server/index.js
+++ src/server/index.js
@@ -12,8 +12,9 @@ import config from './config/environment';
 import webpackConfig from '../../webpack.config.babel';
 
 /* === CONTROLLERS === */
-
+//
 /* === POLICIES === */
+//
 
 if (config.env === 'development') {
   webpackConfig.entry.app.unshift('webpack-dev-server/client?http://localhost:8080/', 'webpack/hot/dev-server');
