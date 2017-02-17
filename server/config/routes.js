68f80f60dcaea1ecaa6a78f69abe6e8ad34e36f4
Donovan Hiland <donovan.hiland@gmail.com>
2017-02-17 12:07:06 -0700

    google verification

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
-// router.get('/', (req, res) => {
-//
-// });
+router.get('/googlede23b79ac9d8f481.html', (req, res) => {
+  res.sendFile(path.resolve(process.cwd(), 'app', 'assets', 'googlede23b79ac9d8f481.html'));
+});
 
 router.get('/api', (req, res) => {
   res.status(200).send('API WORKS');
